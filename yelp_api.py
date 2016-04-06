0f64456a5a84fdd6b734e7469a88a1ac9e6c74b4
Clinton Beasley <cbeasley92@gmail.com>
2016-04-06 16:17:40 -0500

    Init Commit

---
+++ yelp_api.py
@@ -0,0 +1,20 @@
+import io
+import json
+import requests
+
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
+
+# Author:       Clinton Beasley
+# Date:         April 6, 2016
+# Title:        yelp_api.py
+# Description:  Testing integration with the Yelp API
+
+# read API keys from config file
+with io.open('config.json') as cred:
+    creds = json.load(cred)
+    auth = Oauth1Authenticator(**creds)
+    client = Client(auth)
+
+def get_search_dallas():
+    response = requests.get()
