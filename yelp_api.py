9f7812e768968ae4ba43301275a6ef308344b4ab
Clinton Beasley <cbeasley92@gmail.com>
2016-04-07 23:22:50 -0500

    Fixed Yelp API Call Now Searches for "food" in Dallas

--- yelp_api.py
+++ yelp_api.py
@@ -1,6 +1,7 @@
 import io
 import json
-import requests
+import time
+import logging
 
 from yelp.client import Client
 from yelp.oauth1_authenticator import Oauth1Authenticator
@@ -10,11 +11,17 @@ from yelp.oauth1_authenticator import Oauth1Authenticator
 # Title:        yelp_api.py
 # Description:  Testing integration with the Yelp API
 
+# Configure Logging
+formatting = '%(asctime)-15s %(message)s'
+logging.Formatter.converter = time.gmtime
+logging.basicConfig(filename=r'yelp_testing.log', format=formatting, level=logging.INFO)
+
 # read API keys from config file
 with io.open('config.json') as cred:
     creds = json.load(cred)
     auth = Oauth1Authenticator(**creds)
     client = Client(auth)
+    logging.info('Client Authorized')
 
 
 def get_search_dallas():
@@ -23,9 +30,20 @@ def get_search_dallas():
         'lang': 'en'
     }
 
+    logging.info('Searching Yelp in Dallas')
+    logging.info('Passing parameters: %s' % str(params))
+
     response = client.search('Dallas', **params)
 
-    print type(response)
+    # print type(response)
+    # print response.businesses
+
+    print '%-*s%s' % (35, 'Name', 'Rating')
+    logging.info('%-*s%s' % (35, 'Name', 'Rating'))
+
+    for x in response.businesses:
+        print '%-*s%s' % (35, x.name, x.rating)
+        logging.info('%-*s%s' % (35, x.name, x.rating))
 
 
 def main():
